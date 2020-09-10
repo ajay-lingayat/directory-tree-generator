381a654a5d6a2b343d80ef44a7e5811635f796b6
Ajay2810 <lingayatajay2810@gmail.com>
2020-09-10 20:46:05 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'directory-tree-generator',
+    version = '0.0.1',
+    author="Ajay Lingayat",
+    author_email="lingayatajay2810@gmail.com",
+    description = "This module helps to print/generate a directory tree.",
+    url="https://github.com/Ajay2810-hub/directory-tree-generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules = ['DirectoryTree.py'],
+    package_dir = {'': 'src'},
+    install_requires=[], 
+    extras_require = {
+       "dev":[
+       "pytest>=3.7",
+       ]
+    },
+    classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
---
+++ src/DirectoryTree.py
@@ -0,0 +1,24 @@
+import os
+
+class Tree:
+   def __init__( self ):
+       pass
+       
+   def generate( self, path ):
+       if os.path.exists(path):
+          for root, dirs, files in os.walk(path):
+              folder = root.replace(path,'')
+              Count = folder.count(os.sep)
+        
+              spaces = ' '*3*Count
+        
+              print(f'{spaces}+ {os.path.basename(root)}/')
+        
+              for file in files:
+                  sub_space = ' '*4
+                  print(f'{spaces}{sub_space}• {file}')
+                  
+       else:
+          raise FileNotFoundError(f"No such file or directory found \'{path}\'")
+               
+
